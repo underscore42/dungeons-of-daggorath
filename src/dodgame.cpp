underscore42/dungeons-of-daggorath

commit 3ad845ae6faf5463e31eb7e33a49ff7b1e02c5fa

Move dungeon seeding code
Moved the code from OS_Link::init to dodGame::COMINI.

src/dodgame.cpp
@@ -79,6 +79,21 @@ void dodGame::COMINI()
     viewer.VXSCALf = 128.0f;
     viewer.VYSCALf = 128.0f;
     AUTFLG = viewer.ShowFade(Viewer::FADE_BEGIN);
+    IsDemo = AUTFLG;
+    if (IsDemo)
+    {
+        LEVEL = 2;
+        dungeon.SetLEVTABOrig();  //Make sure the original seeds aren't overwritten from pervious new game.
+    }
+    else
+    {
+        LEVEL = 0;
+        if (!RandomMaze)
+            dungeon.SetLEVTABOrig();  //Make sure the original seeds aren't overwritten from pervious new game.
+        else
+            dungeon.SetLEVTABRandomMap();  //Sets random seeds for maze.
+    }
+
     player.setInitialObjects(AUTFLG);
     viewer.displayPrepare();
     viewer.display_mode = Viewer::MODE_TITLE;

src/oslink.cpp
@@ -153,7 +153,6 @@ void OS_Link::init()
                 {
                     // Restart demo
                     game.hasWon = false;
-                    game.DEMOPTR = 0;
                     object.Reset();
                     creature.Reset();
                     parser.Reset();

commit 04075fbfe3c0da270f70fa3b8eb8517a63355c21

Fix bugs introduced by 3ad845ae6faf

src/dodgame.cpp
@@ -82,6 +82,8 @@ void dodGame::COMINI()
     IsDemo = AUTFLG;
     if (IsDemo)
     {
+        hasWon = false;
+        DEMOPTR = 0;
         LEVEL = 2;
         dungeon.SetLEVTABOrig();  //Make sure the original seeds aren't overwritten from pervious new game.
     }
@@ -139,6 +141,12 @@ void dodGame::Restart()
     viewer.Reset();
     hasWon = false;
 
+    LEVEL = 0;
+    if (!RandomMaze)
+        dungeon.SetLEVTABOrig();
+    else
+        dungeon.SetLEVTABRandomMap();
+
     dungeon.VFTPTR = 0;
     scheduler.SYSTCB();
     object.CreateAll();
@@ -157,7 +165,7 @@ void dodGame::Restart()
         oslink.process_events();
         ticks2 = SDL_GetTicks();
     }
-    while (ticks2 < ticks1 + 2500);
+    while (ticks2 < ticks1 + viewer.prepPause);
 
     creature.NEWLVL();
     INIVU();

src/oslink.cpp
@@ -152,7 +152,6 @@ void OS_Link::init()
                 if (game.demoRestart)
                 {
                     // Restart demo
-                    game.hasWon = false;
                     object.Reset();
                     creature.Reset();
                     parser.Reset();